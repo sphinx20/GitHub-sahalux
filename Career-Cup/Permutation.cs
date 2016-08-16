13ad311b3eeedec84647f3516838412e3030ac35
Luxmi Saha <sphinx20@gmail.com>
2016-08-15 23:35:06 -0400

    Solutions from Career cup book

---
+++ Career-Cup/Permutation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Personal_Projects
+{
+    public class Permutation
+    {
+        public void Start()
+        {
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            if (str1.Length != str2.Length)
+            {
+                Console.WriteLine("Strings are not permutation.");
+                Environment.Exit(0);
+            }
+            int[] arr1 = new int[26];
+            int[] arr2 = new int[26];
+
+            for (int i = 0; i < 26; i++)
+            {
+                arr1[i] = arr2[i] = 0;
+            }
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                int c = str1[i] - 97;
+                arr1[c] = arr1[c] + 1;
+
+                c = str2[i] - 97;
+                arr2[c] = arr2[c] + 1;
+
+            }
+            var check = true;
+            for (int i = 0; i < 26; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    check = false;
+                   
+                }
+            }
+            Console.WriteLine(check ? "Strings are permutation." : "Strings are not permutation.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Career-Cup/UniqueChar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Personal_Projects
+{
+    public class UniqueChar
+    {
+        public void Start()
+        {
+            var str = Console.ReadLine();
+
+            var check = SolutionFromBook(str);
+
+            Console.WriteLine(check ? "All characters are unique" : "All characters are not unique");
+
+            Console.ReadLine();
+
+        }
+
+        private void UniqueWithDataStructure(string str)
+        {
+            var arr = new Hashtable();
+            foreach (var ch in str)
+            {
+                if (arr.ContainsKey(ch))
+                {
+                    arr[ch] = false;
+                }
+                else
+                {
+                    arr.Add(ch, true);
+                }
+            }
+            foreach (var ch in str)
+            {
+                if ((bool)arr[ch])
+                {
+                    Console.WriteLine($"{ch}: Value is unique.");
+                }
+                else
+                {
+                    Console.WriteLine($"{ch}: Value is not unique.");
+                }
+            }
+        }
+
+        private bool SolutionFromBook(string str)
+        {
+            int checker = 0;
+            foreach (var val in str)
+            {
+                if ((checker & (1 << val)) > 0)
+                {
+                    return false;
+                }
+                checker = (checker | (1 << val));
+            }
+
+            return true;
+
+        }
+    }
+}
